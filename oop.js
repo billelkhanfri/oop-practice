2465dbe08bb7917746d0b3760fc8cf0d245f1ddb
billelkhanfri <khanfri.billel@yahoo.fr>
2023-06-20 09:33:48 +0200

    oop practice

--- oop.js
+++ oop.js
@@ -17,3 +17,50 @@ let user = {
   },
 };
 console.log(user.adresses.getmainadress("billel"));
+let mayVarirable = "billel";
+let myObject = {
+  billel: "is my name",
+};
+console.log(myObject[mayVarirable]);
+
+/* practice objects**/
+
+const book = {
+  title: "The Great Gatsby",
+  author: "F. Scott Fitzgerald",
+  year: 1925,
+  genre: "Fiction",
+  getSummary: function () {
+    return `${this.title} by ${this.author}, published in ${this.year}.`;
+  },
+};
+
+// Access object properties
+console.log(book.title); // Output: The Great Gatsby
+console.log(book.author); // Output: F. Scott Fitzgerald
+
+// Call object method
+console.log(book.getSummary()); // Output: The Great Gatsby by F. Scott Fitzgerald, published in 1925.
+// assign method
+let newBook = Object.assign({}, book, { plot: "nice book" });
+console.log(newBook);
+let userProfile = {
+  name: "user name",
+  adress: "user adress",
+  email: "user email",
+  age: 26,
+};
+function updateProfile(newInfo) {
+  return Object.assign(userProfile, newInfo);
+}
+let newUser = {
+  name: "Billel khanfri",
+  adress: "france",
+  email: "khanfri@gmail.com",
+  age: 40,
+};
+console.log(updateProfile(newUser));
+
+/* delete object property*/
+delete userProfile.age;
+console.log(userProfile);
